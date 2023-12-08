e9879e3e63e1ee7584ac6550ccacc429e12a29e9
Justas Stankevičius <justas.stankevicius@stud.kitm.lt>
2023-12-08 20:05:57 +0200

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import {Route, Routes} from 'react-router-dom'
 
 import Home from './Pages/Home'
 
@@ -8,9 +7,7 @@ import Home from './Pages/Home'
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home />}> </Route>
-    </Routes> 
+    <Home />
     </>
   )
 }
