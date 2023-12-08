9515c803c2b919b301296259df41a1c7cf913d90
Justas Stankevičius <justas.stankevicius@stud.kitm.lt>
2023-12-08 19:46:06 +0200

    deploy coiny

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import {Route, Routes} from 'react-router-dom'
 
+import Home from './Pages/Home'
 
-import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
-      <Outlet/>
+    <Routes>
+      <Route path='/' element={<Home />}> </Route>
+    </Routes> 
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {BrowserRouter } from 'react-router-dom'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Home from './Pages/Home'
-
-const  router = createBrowserRouter([
-  {
-    path:"/Currency/",
-    element: <App/>,
-    children:[
-      {
-        path:"/Currency/",
-        element:<Home/>
-      }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
+    <BrowserRouter>
       <App />    
-    </RouterProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
