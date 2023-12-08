a13ae67515c55ff8e4794bb13fa408628320dbb5
Justas Stankevičius <justas.stankevicius@stud.kitm.lt>
2023-12-08 18:44:27 +0200

    push test

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import {RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Home from './Pages/Home'
+
+const  router = createBrowserRouter([
+  {
+    path:"/Currency/",
+    element: <App/>,
+    children:[
+      {
+        path:"/Currency/",
+        element:<Home/>
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <RouterProvider router={router}>
       <App />    
-    </BrowserRouter>
+    </RouterProvider>
   </React.StrictMode>,
 )
