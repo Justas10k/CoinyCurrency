55884c2419f111b265cfba5748b0aabb6367fe18
Justas Stankevičius <justas.stankevicius@stud.kitm.lt>
2023-12-09 18:03:00 +0200

    More collumn symbol, name working

--- src/Components/LiveRates.jsx
+++ src/Components/LiveRates.jsx
@@ -10,13 +10,14 @@ function LiveRates() {
   const [selectedNewCurrency, setSelectedNewCurrency] = useState('');
   const [currencyOptions, setCurrencyOptions] = useState([]);
 
+  const [currenciesInfo, setCurrenciesInfo] = useState({});
+
   const [inverse, setInverse] = useState(false);
   const toggleVerse = () => {
     setInverse(!inverse);
   };
   
 
-  
   // Function to handle currency deletion
   const handleDelete = (currency) => {
     setRowsToDelete((prevRows) => [...prevRows, currency]);
@@ -27,6 +28,7 @@ function LiveRates() {
     setRowsToDelete([]);
     setShowDeleteButton(false);
   };
+
   // Function to handle addition of a new currency
   const handleAddCurrency = () => {
     if (selectedNewCurrency && !currencies.includes(selectedNewCurrency)) {
@@ -64,21 +66,32 @@ function LiveRates() {
       .catch((error) => console.error('Error fetching exchange rates:', error));
   };
 
-  useEffect(() => {
-    const apiUrl = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_5xYPjNyOkAtaVLiwQjepWvuYGg8jQPpDt2MQah02&base_currency=${selectedCurrency}`;
+useEffect(() => {
+  const apiUrl = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_5xYPjNyOkAtaVLiwQjepWvuYGg8jQPpDt2MQah02&base_currency=${selectedCurrency}`;
 
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        setExchangeRates(data.data);
-        const options = Object.keys(data.data);
-        const filteredOptions = options.filter(item => !selectedCurrency.includes(item) && !currencies.includes(item));
-        setCurrencyOptions(filteredOptions);
-      })
-      .catch((error) => console.error('Error fetching exchange rates:', error));
-  }, [selectedCurrency, currencies]);
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      setExchangeRates(data.data);
+      const options = Object.keys(data.data);
+      const filteredOptions = options.filter(item => !selectedCurrency.includes(item) && !currencies.includes(item));
+      setCurrencyOptions(filteredOptions);
+    })
+    .catch((error) => console.error('Error fetching exchange rates:', error));
+}, [selectedCurrency, currencies]);
 
+useEffect(() => {
+  // Assuming you're making an HTTP request to the API
+  const apiUrl = "https://api.freecurrencyapi.com/v1/currencies?apikey=fca_live_GOADoaUR68WLhGybdVUrPgfquKykdmAfQMX8yP6h";
 
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const currenciesInfo = data.data;
+      setCurrenciesInfo(currenciesInfo); // Set the state with the received data
+    })
+    .catch(error => console.error("Error fetching data:", error));
+}, []);
   return (
     <>
       <div id="market">
@@ -87,13 +100,15 @@ function LiveRates() {
           <div className="market-table">
             <div className="table-row">
               <div className="checkbox-container full-width table-padding-right ">
-                <p className=''>Inverse</p>
+                <p className='p-1'>Inverse</p>
                 <label className="switch" >
                   <input type="checkbox" onClick={toggleVerse}/>
                   <span className="slider round"></span>
                 </label>
               </div>
               <p className='full-width'>Amount</p>
+              <p className='full-width'>Symbol</p>
+              <p className='full-width'>Full name</p>
               <div className='full-width-end right-side table-padding-left'>
               <button className="Table-edit " onClick={showDeleteButton ? handleDone : setShowDeleteButton}>
                 {showDeleteButton ? 'Done' : 'Edit'}
@@ -102,12 +117,15 @@ function LiveRates() {
             </div>
 
             <div className="table-row select-table">
-              <p className='full-width select-currency table-padding-right'>{selectedCurrency}</p>
-              <p className='full-width'>{inverse ? 'inverse' : '1'}</p>
-              <p className='full-width table-padding-left full-width-end'></p>
-            </div>
 
-            {currencies.map((currency) => (
+            <p className='full-width select-currency table-padding-right'>{selectedCurrency}</p>
+            <p className='full-width'>{inverse ? 'inverse' : '1'}</p>
+            <p className='full-width'>{currenciesInfo[selectedCurrency]?.symbol}</p>
+            <p className='full-width'>{currenciesInfo[selectedCurrency]?.name}</p>
+            <p className='full-width table-padding-left full-width-end'></p>
+          </div>
+            
+{currencies.map((currency) => (
   !rowsToDelete.includes(currency) && (
     <div className="table-row table-border" key={currency}>
       <p
@@ -122,6 +140,8 @@ function LiveRates() {
           : 'Loading...'
         }
       </p>
+      <p className='full-width'>{currenciesInfo[currency]?.symbol}</p>
+      <p className='full-width'>{currenciesInfo[currency]?.name}</p>
       <div className='full-width-end right-side table-padding-left'>
         <button
           className={`max-width  ${showDeleteButton ? 'delete-button' : 'hidden-button'}`}
